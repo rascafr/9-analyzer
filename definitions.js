fb191193572075100929414ee1dfb5c4456bd58a
rascafr <rascafr@gmail.com>
2019-08-30 17:18:36 +0200

    ~ definitons as hex

--- definitions.js
+++ definitions.js
@@ -1,25 +1,25 @@
 const D = module.exports;
 
 D.headerBytes = [
-    '5A', 'A5'
+    0x5a, 0xa5
 ]
 
 D.addresses = {
-    '20': 'ESC',
-    '21': 'BLE',
-    '22': 'BMS',
-    '23': 'Ext-BMS',
-    '00': '?-ESC',
-    '01': '?',
-    '3D': 'APP',
-    '3E': 'APP',
-    '3F': 'APP',
+    0x20: 'ESC',
+    0x21: 'BLE',
+    0x22: 'BMS',
+    0x23: 'Ext-BMS',
+    0x00: '?-ESC',
+    0x01: '?',
+    0x3d: 'APP',
+    0x3e: 'APP',
+    0x3f: 'APP',
 }
 
 D.commands = {
-    '1':  'Ask read registers',
-    '4':  'Response read registers',
-    '61': 'Read registers, update head',
-    '64': 'Update head display',
-    '65': 'Update head sensors'
+    0x1:  'Ask read registers',
+    0x4:  'Response read registers',
+    0x61: 'Read registers, update head',
+    0x64: 'Update head display',
+    0x65: 'Update head sensors'
 }
