09c0fc01b35e107708650c109707bc6842f212c9
rascafr <rascafr@gmail.com>
2019-09-02 14:40:39 +0200

    + plantuml code call, cleaned index

--- index.js
+++ index.js
@@ -1,18 +1,36 @@
 const fs = require('fs');
 const clc = require('cli-color');
-const sha1 = require('js-sha1');
 const blue = clc.blueBright;
-const D = require('./definitions');
 const Parser = require('./parser');
+const PlantUML = require('./plantuml');
 
 const INPUT_FILE_LOG = `unlockThenRideHexNoParsingV2.log`;
 const INPUT_PATH_LOG = `${__dirname}/logfiles/${INPUT_FILE_LOG}`;
-const LOG_HEADER = 'Ready...';
 
+// UML file (generated only if PLANTUML_PATH env variable exists)
+const UML_FILE_LOG = `uml.txt`;
+const UML_PATH_LOG = `${__dirname}/generated/${UML_FILE_LOG}`;
+
+// Verbose file
 const OUTPUT_FILE_LOG = `resume.log`;
-const OUTPUT_PATH_LOG = `${__dirname}/${OUTPUT_FILE_LOG}`;
+const OUTPUT_PATH_LOG = `${__dirname}/generated/${OUTPUT_FILE_LOG}`;
+
+const PLANTUML_PATH = process.env.PLANTUML_PATH;
 
 console.log(blue('Reading file'), INPUT_PATH_LOG);
 
 const logData = fs.readFileSync(INPUT_PATH_LOG)
-Parser.parseHex(logData);
+let frames = Parser.parseHex(logData);
+let txt = '';
+frames.forEach(frame => {
+    txt += Parser.frameToString(frame) + '\n';
+});
+fs.writeFileSync(OUTPUT_PATH_LOG, txt);
+
+// generate uml sequence diagram?
+if (PLANTUML_PATH) {
+    let uml = PlantUML.toUML(frames);
+    fs.writeFileSync(UML_PATH_LOG, uml);
+    PlantUML.generateImage(PLANTUML_PATH, UML_PATH_LOG);
+} else console.log('UML file skipped');
+
